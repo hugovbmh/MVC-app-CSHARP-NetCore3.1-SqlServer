50d16d53971675a7364522d5847a5044f7ebf4aa
Hugo <hugovilchezbeltran@gmail.com>
2022-11-24 10:04:50 -0500

    MVC APP

---
+++ CursoMVC2022/Controllers/UserController.cs
@@ -0,0 +1,32 @@
+﻿using CursoMVC2022.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CursoMVC2022.Controllers
+{
+    public class UserController : Controller
+    {
+        
+        public IActionResult Index()
+        {
+            List<User> lista = new List<User>();
+            using (var db = new Models.DB.CursoMvcCoreContext())
+            {
+                lista = (from d in db.User
+                         select new User
+                         {
+                             Id = d.Id,
+                             Email = d.Email
+                         }).ToList();
+            }
+            return View(lista);
+        }
+    }
+}
+
+//var user = new User();
+//user.Name = "Hugo";
+//user.Email = "hugovb@gmail.com";
---
+++ CursoMVC2022/Models/DB/CursoMvcCoreContext.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace CursoMVC2022.Models.DB
+{
+    public partial class CursoMvcCoreContext : DbContext
+    {
+        public CursoMvcCoreContext()
+        {
+        }
+
+        public CursoMvcCoreContext(DbContextOptions<CursoMvcCoreContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<User> User { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Server=localhost; Database=CursoMvcCore; Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.ToTable("user");
+
+                entity.Property(e => e.Id).HasColumnName("id");
+
+                entity.Property(e => e.Date)
+                    .HasColumnName("date")
+                    .HasColumnType("datetime");
+
+                entity.Property(e => e.Email)
+                    .IsRequired()
+                    .HasColumnName("email")
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Password)
+                    .IsRequired()
+                    .HasColumnName("password")
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
